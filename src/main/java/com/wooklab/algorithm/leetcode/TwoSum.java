c2b467ff5a57ebf3d05c3942de4112e51cbd40f8
Inwook Hwang <inwook9@gmail.com>
2020-01-08 01:32:39 +0900

    added 'TwoSum'

--- src/main/java/com/wooklab/algorithm/leetcode/TwoSum.java
+++ src/main/java/com/wooklab/algorithm/leetcode/TwoSum.java
@@ -1,6 +1,7 @@
 package com.wooklab.algorithm.leetcode;
 
 /**
+ * 1. TwoSum
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *
@@ -10,4 +11,15 @@ package com.wooklab.algorithm.leetcode;
  * return [0, 1].
  */
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[] {};
+    }
 }
