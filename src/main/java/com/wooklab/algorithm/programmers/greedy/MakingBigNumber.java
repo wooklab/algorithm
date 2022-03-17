a504fc80183c02786a3057ad53e32ed741c30fb6
Hwang In-wook <inwook9@gmail.com>
2022-03-18 01:23:04 +0900

    스터디) 알고리즘

--- src/main/java/com/wooklab/algorithm/programmers/greedy/MakingBigNumber.java
+++ src/main/java/com/wooklab/algorithm/programmers/greedy/MakingBigNumber.java
@@ -1,6 +1,6 @@
 package com.wooklab.algorithm.programmers.greedy;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42883
@@ -10,82 +10,56 @@ public class MakingBigNumber {
     public static void main(String[] args) {
         MakingBigNumber m = new MakingBigNumber();
 
-        // 시간초과
-//        System.out.println(m.solution("1924", 2));          // answer: "94"
-//        System.out.println(m.solution("1231234", 3));       // answer: "3234"
-//        System.out.println(m.solution("4177252841", 4));    // answer: "775841"
+        System.out.println(m.solution("1924", 2));          // answer: "94"
+        System.out.println(m.solution("1231234", 3));       // answer: "3234"
+        System.out.println(m.solution("4177252841", 4));    // answer: "775841"
+        System.out.println(m.solution("1", 1));    // answer: ""
+        System.out.println(m.solution("111112", 4));    // answer: "12"
+        System.out.println(m.solution("333111333", 3));    // answer: "3333333"
 
+        System.out.println("==");
         System.out.println(m.solution2("1924", 2));          // answer: "94"
         System.out.println(m.solution2("1231234", 3));       // answer: "3234"
         System.out.println(m.solution2("4177252841", 4));    // answer: "775841"
-        System.out.println(m.solution2("1", 1));    // answer: "775841"
-
+        System.out.println(m.solution2("1", 1));    // answer: ""
+        System.out.println(m.solution2("111112", 4));    // answer: "12"
+        System.out.println(m.solution2("333111333", 3));    // answer: "3333333"
     }
+
 
     public String solution(String number, int k) {
-        int[] numbers = Arrays.stream(number.split("")).mapToInt(Integer::parseInt).toArray();
-        int answer = combine(numbers, new boolean[numbers.length], 0, 0, numbers.length - k);
-        return String.valueOf(answer);
-    }
+        StringBuilder answer = new StringBuilder();
 
-    private int combine(int[] numbers, boolean[] visited, int startIndex, int depth, int maxDepth) {
-        int maxNumber = 0;
-        if (depth == maxDepth) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < numbers.length; i++) {
-                if (visited[i]) {
-                    sb.append(numbers[i]);
+        int startIndex = 0;
+        for (int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for (int j = startIndex; j <= k + i; j++) {
+                int current = number.charAt(j) - '0';
+                if (max < current) {
+                    max = current;
+                    startIndex = j + 1;
                 }
             }
-            return Integer.parseInt(sb.toString());
-        } else {
-            for (int i = startIndex; i < numbers.length; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    int returnedNumber = combine(numbers, visited, i + 1, depth + 1, maxDepth);
-                    maxNumber = Math.max(maxNumber, returnedNumber);
-                    visited[i] = false;
-                }
-            }
+            answer.append(max);
         }
-        return maxNumber;
+        return answer.toString();
     }
 
+    // 베스트 솔루션
     public String solution2(String number, int k) {
-        if (number.length() == k) {
-            return "0";
-        }
-        int length = number.length() - k;
-        String targetNumber = number.substring(0, length);
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
 
-        int cnt = 0;
-        for (int i = length; i < number.length(); i++) {
-            if (cnt == k) {
-                break;
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
             }
-            targetNumber = removeMinNumber(targetNumber) + number.charAt(i);
-            cnt++;
+            stack.push(c);
         }
-        return targetNumber;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
     }
-
-    private String removeMinNumber(String targetNumber) {
-        int[] numbers = Arrays.stream(targetNumber.split("")).mapToInt(Integer::parseInt).toArray();
-        int minValue = Integer.MAX_VALUE;
-        int minIndex = 0;
-        for (int i = 0; i < targetNumber.length(); i++) {
-            if (minValue > numbers[i]) {
-                minValue = numbers[i];
-                minIndex = i;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < numbers.length; i++) {
-            if (i != minIndex) {
-                sb.append(numbers[i]);
-            }
-        }
-        return sb.toString();
-    }
-
 }
